cc2695929d808ec46d8dad25d89166cf7fe77fed
adnan-mkhtr <adnanmuchtar8e01@gmail.com>
2023-12-10 11:17:11 +0700

    add new change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,22 @@
 let navbar = document.getElementById("nav-menu"),
 navToggle = document.getElementById("nav-toggle"),
-navLink = document.querySelectorAll(".nav__link");
+navLink = document.querySelectorAll(".nav__link"),
+sidebarToggle = document.getElementById("sidebar-toggle"),
+sideBar = document.getElementById("sidebar");
 navToggle.addEventListener("click", () =>{
     navbar.classList.toggle("active");
+
+    if (sideBar.classList.contains("active")){
+        sideBar.classList.remove("active");
+    }
+})
+
+sidebarToggle.addEventListener("click", () =>{
+    sideBar.classList.toggle("active");
+
+    if (navbar.classList.contains("active")){
+        navbar.classList.remove("active");
+    }
 })
 
 navLink.forEach((link) => {
@@ -36,4 +50,19 @@ function activeMenuItem(){
     this.classList.add("active");
 }
 
-menuItems.forEach((item) => item.addEventListener("click", activeMenuItem))
+menuItems.forEach((item) => item.addEventListener("click", activeMenuItem));
+
+// Animation scroll by scroll reveal
+
+const sr = ScrollReveal({
+    distance: "100px",
+    duration: 2000,
+    delay: 400,
+});
+
+sr.reveal(".home__content");
+sr.reveal(".img_home", {origin: "top"});
+sr.reveal(".feature__card", {interval:200});
+sr.reveal(".img_about", {origin: "left"});
+sr.reveal(".about__content", {origin: "right"});
+sr.reveal(".review__card, .blog__post, .footer__content", {interval: 200});
